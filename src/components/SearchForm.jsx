19e2bc823e4569dbb2c803953801e4c3fc8eeae4
Stuart Gill <stuart.a.gill@gmail.com>
2019-03-01 15:14:50 -0800

    add search form component with functioning api call, which requires exact latitude longitude and currently only console logs the entire json object of the weather forecast returned

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function SearchForm() {
-  return (
-    <div>
-      <h1>Search Form works</h1>
-    </div>
-  )
+export default class SearchForm extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      location: ''
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    //let location = "47.6062%2C-122.3321";
+    const search = {
+      location: this.state.location
+    }
+    let url= `https://api.weather.gov/points/${search.location}/forecast`
+    
+    fetch(url, {
+      method: 'GET',
+    })
+    .then(res => res.json())
+    .then(data => console.log(data));
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>New Search</h1>
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <label>Location in Lat and Long</label><br/>
+            <input type="text" name="location" onChange = {this.onChange} value={this.state.location}/>
+          </div>
+          <br/>
+          <button type="submit"> Submit </button>
+        </form>
+      </div>
+    )
+  }
 }
+
