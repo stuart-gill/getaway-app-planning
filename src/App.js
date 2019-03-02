956f6ac6210bcb968462636cd410f9a7b0ab69ad
Stuart Gill <stuart.a.gill@gmail.com>
2019-03-01 22:33:53 -0800

    successfully returns temperature over next 14 periods, puts this info in the state, and then displays it. Next state needs to be raised to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Contact from './components/Contact';
-import Header from './components/Header';
-import ResultItem from './components/ResultItem';
-import ResultsList from './components/ResultsList';
-import SearchForm from './components/SearchForm';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import ResultItem from "./components/ResultItem";
+import ResultsList from "./components/ResultsList";
+import SearchForm from "./components/SearchForm";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: []
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -16,10 +22,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <Contact />
           <Header />
-          <ResultItem/>
-          <ResultsList/>
-          <SearchForm/>
-
+          <ResultItem />
+          <ResultsList />
+          <SearchForm />
         </header>
       </div>
     );
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from 'react'
-import { isContext } from 'vm';
+import React, { Component } from "react";
 
 export default class SearchForm extends Component {
-  
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      location: ''
+      location: "",
+      weather: []
     };
 
     this.onChange = this.onChange.bind(this);
@@ -14,7 +13,7 @@ export default class SearchForm extends Component {
   }
 
   onChange(e) {
-    this.setState({[e.target.name]: e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   onSubmit(e) {
@@ -22,30 +21,44 @@ export default class SearchForm extends Component {
     //let location = "47.6062%2C-122.3321";
     const search = {
       location: this.state.location
-    }
-    let url= `https://api.weather.gov/points/${search.location}/forecast`
-    
+    };
+    let url = `https://api.weather.gov/points/${search.location}/forecast`;
+
     fetch(url, {
-      method: 'GET',
+      method: "GET"
     })
-    .then(res => res.json())
-    .then(data => console.log(data.properties.periods));
+      .then((res) => res.json())
+      // .then((data) => console.log(data.properties.periods));
+      .then((data) => this.setState({ weather: data.properties.periods }));
   }
-  
+
   render() {
+    const weatherItems = this.state.weather.map((item) => (
+      <div key={item.number}>
+        <h3>{item.name}</h3>
+        <p>{item.temperature}</p>
+      </div>
+    ));
+
     return (
       <div>
         <h1>New Search</h1>
         <form onSubmit={this.onSubmit}>
           <div>
-            <label>Location in Lat and Long</label><br/>
-            <input type="text" name="location" onChange = {this.onChange} value={this.state.location}/>
+            <label>Location in Lat and Long</label>
+            <br />
+            <input
+              type="text"
+              name="location"
+              onChange={this.onChange}
+              value={this.state.location}
+            />
           </div>
-          <br/>
+          <br />
           <button type="submit"> Submit </button>
         </form>
+        {weatherItems}
       </div>
-    )
+    );
   }
 }
-
