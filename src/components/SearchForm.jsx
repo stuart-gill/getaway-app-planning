be53457f7782c7b26dc65be99eac2b966580ba18
Stuart Gill <stuart.a.gill@gmail.com>
2019-03-03 10:10:06 -0800

    add function in SearchForm component to calculate acceptable city from travel time input. Not currently working because of async problems, API call happens before city is selected. Also only works with one city, need to add funcitonality to cycle through acceptable cities

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,28 +1,90 @@
 import React, { Component } from "react";
 
+const cityList = [
+  {
+    id: 1,
+    name: "Leavenworth",
+    hours: 2.5,
+    latlong: "47.5962%2C-120.6615"
+  },
+  {
+    id: 2,
+    name: "Yakima",
+    hours: 2.2,
+    latlong: "46.6021%2C-120.5059"
+  },
+  {
+    id: 3,
+    name: "Tacoma",
+    hours: 0.75,
+    latlong: "47.2529%2C-122.4443"
+  }
+];
+
 export default class SearchForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       location: "",
+      travelTime: "",
       weather: []
     };
 
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.onSubmitHours = this.onSubmitHours.bind(this);
+    this.determineSuitableCities = this.determineSuitableCities.bind(this);
   }
 
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  determineSuitableCities(travelTime) {
+    for (let i = 0; i < cityList.length; i++) {
+      if (cityList[i].hours < travelTime) {
+        this.setState({ location: cityList[i].latlong });
+        console.log(`determine cities set state to ${i}`);
+      } else {
+        this.setState({ location: "" });
+      }
+      console.log(
+        `travel time is ${travelTime} determine cities ran time ${i}`
+      );
+    }
+  }
+
   onSubmit(e) {
     e.preventDefault();
-    //let location = "47.6062%2C-122.3321";
     const search = {
       location: this.state.location
     };
     let url = `https://api.weather.gov/points/${search.location}/forecast`;
+
+    fetch(url, {
+      method: "GET"
+    })
+      .then((res) => res.json())
+      .then((data) => this.setState({ weather: data.properties.periods }));
+  }
+
+  onSubmitHours(e) {
+    e.preventDefault();
+    this.determineSuitableCities(this.state.travelTime);
+    //let location = "47.6062%2C-122.3321";
+
+    // const travelTime = this.state.travelTime;
+    // for (let i = 0; i < cityList.length; i++) {
+    //   if (cityList[i].hours < travelTime) {
+    //     this.setState({ location: cityList[i].latlong });
+    //   }
+    // }
+
+    const search = {
+      location: this.state.location
+    };
+    let url = `https://api.weather.gov/points/${search.location}/forecast`;
+    console.log(url);
 
     fetch(url, {
       method: "GET"
@@ -57,6 +119,22 @@ export default class SearchForm extends Component {
           <br />
           <button type="submit"> Submit </button>
         </form>
+
+        <form onSubmit={this.onSubmitHours}>
+          <div>
+            <label>Hours willing to travel</label>
+            <br />
+            <input
+              type="number"
+              name="travelTime"
+              onChange={this.onChange}
+              value={this.state.travelTime}
+            />
+          </div>
+          <br />
+          <button type="submit"> Submit </button>
+        </form>
+
         {weatherItems}
       </div>
     );
