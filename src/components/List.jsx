60a35efa79b2c051dc3395219d397177d58bce9e
Stuart Gill <stuart.a.gill@gmail.com>
2019-03-09 13:51:24 -0800

    list out sorted cities from redux store state into list component, everything working now except api calls, which need to get transitioned to middleware

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import { connect } from "react-redux";
 
 const mapStateToProps = (state) => {
-  return { reduxWeather: state.reduxWeather };
+  return { reduxWeather: state.reduxWeather, sortedCities: state.sortedCities };
 };
 
-const ConnectedList = ({ reduxWeather }) => (
-  <p>Redux weather from List comp: {reduxWeather}</p>
+const ConnectedList = ({ reduxWeather, sortedCities }) => (
+  <div>
+    <p>Redux weather from List comp: {reduxWeather}</p>
+    <ul className="list-group list-group-flush">
+      {sortedCities.map((el) => (
+        <li className="list-group-item" key={el.id}>
+          Sorted city: {el.name}
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 
 const List = connect(mapStateToProps)(ConnectedList);
