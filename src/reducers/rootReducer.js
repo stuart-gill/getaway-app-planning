ae836f7b3959225b95575bbcfcf6cadf6a3d0c13
stuart gill <stuart.a.gill@gmail.com>
2019-03-13 20:59:49 -0700

    last commit of day

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import reduxWeather from "./reduxWeather";
 import sortedCities from "./sortedCities";
 import selectCity from "./selectCity";
+import fetchWeather from "./fetchWeather";
 
 export default combineReducers({
   reduxWeather,
   sortedCities,
-  selectCity
+  selectCity,
+  fetchWeather
 });
