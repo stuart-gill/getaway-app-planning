88e9ab7edb9f3a47b789b3dcdbe4c0acdffffda8
Stuart Gill <stuart.a.gill@gmail.com>
2019-05-27 09:51:32 -0700

    refines card content

--- src/components/WeatherListCard.jsx
+++ src/components/WeatherListCard.jsx
@@ -39,7 +39,7 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export default function WeatherListDisplay(props) {
+export default function WeatherListCard(props) {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
   const weatherAndTemp = `${props.daysWeather.temperature} + ${
@@ -71,11 +71,11 @@ export default function WeatherListDisplay(props) {
         image={props.daysWeather.icon}
         title="Weather pic"
       />
-      <CardContent>
+      {/* <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
           {props.daysWeather.detailedForecast}
         </Typography>
-      </CardContent>
+      </CardContent> */}
       <CardActions disableSpacing>
         <IconButton aria-label="Add to favorites">
           <FavoriteIcon />
@@ -95,26 +95,9 @@ export default function WeatherListDisplay(props) {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Some details</Typography>
           <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum
+            {props.daysWeather.detailedForecast}
           </Typography>
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum
-          </Typography>
-          <Typography>And that's it folks</Typography>
         </CardContent>
       </Collapse>
     </Card>
